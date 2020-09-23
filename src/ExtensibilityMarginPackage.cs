0e6567e25c7df75bd8d41ddb091ae6719919b7b1
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-09-23 11:43:47 -0700

    Cleanup

--- src/ExtensibilityMarginPackage.cs
+++ src/ExtensibilityMarginPackage.cs
@@ -1,23 +1,22 @@
-﻿using Microsoft.VisualStudio.Shell;
-
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-
+using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
 namespace ExtensibilityMargin
 {
-	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-	[Guid(PackageGuids.guidPackageString)]
-	[ProvideProfile(typeof(DialogPageProvider.General), "Environment", Vsix.Name, 0, 0, true)]
-	[ProvideMenuResource("Menus.ctmenu", 1)]
-	public sealed class ExtensibilityMarginPackage : AsyncPackage
-	{
-		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-		{
-			await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-		    await MarginToggleCommand.InitializeAsync(this);
-		}
-	}
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(PackageGuids.guidPackageString)]
+    [ProvideProfile(typeof(DialogPageProvider.General), "Environment", Vsix.Name, 0, 0, true)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class ExtensibilityMarginPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await MarginToggleCommand.InitializeAsync(this);
+        }
+    }
 }
