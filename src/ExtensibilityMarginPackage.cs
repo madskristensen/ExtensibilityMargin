b7c0859abfdf2197e9a895cab439562d36828634
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-04-23 15:35:32 -0700

    Added ProvideBindingPath attribute

--- src/ExtensibilityMarginPackage.cs
+++ src/ExtensibilityMarginPackage.cs
@@ -10,7 +10,8 @@ namespace ExtensibilityMargin
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(PackageGuids.guidPackageString)]
     [ProvideProfile(typeof(DialogPageProvider.General), "Environment", Vsix.Name, 0, 0, true)]
-    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]    
+    [ProvideBindingPath()]
     public sealed class ExtensibilityMarginPackage : AsyncPackage
     {
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
